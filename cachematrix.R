155856609335253ff731cf1c34883ca168c39bfc
richwkc <wongrh@TDD00671119.TDBFG.com>
2015-07-17 17:42:04 -0400

    Revert "branch1"
    This reverts commit af13b23e13b6e9a4047b28abb87bcbb8babbb6c9.

--- cachematrix.R
+++ cachematrix.R
@@ -1,40 +1,15 @@
 ## Put comments here that give an overall description of what your
-## functions cache inverse of matrix
+## functions do
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) 
-{
-	m <- NULL
-	get <- function() x
-	set <- function(y)
-	{	
-		x <<- y
-		m <<- NULL
-	}
+makeCacheMatrix <- function(x = matrix()) {
 
-	#cache solved inverse matrix	
-	setinverse <- function(inverse) m <<- inverse
-	getinverse <- function() m
-	list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)	
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) 
-{
-      ## Return a matrix that is the inverse of 'x'
-		
-	m <- x$getinverse()
-	if(!is.null(m))
-	{	
-		message('getting cached inverse')
-		return(m)
-	}
-
-	data <- x$get()
-	m <- solve(data, ...)
-	x$setinverse(m)	
-	m
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
 }
